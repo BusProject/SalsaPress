4095e925659e1d4d974d794c9ebd0664785b3555
Scott Duncombe <srduncombe@gmail.com>
2012-03-05 11:24:57 -0800

    Fixing Authentication Bug

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@ Plugin Name: SalsaPress
 Plugin URI: https://github.com/BusProject/SalsaPress
 Description: SalsaPress connects WordPress to Salsa
 Author: Scott Duncombe
-Version: 1.1
+Version: 1.11
 Author URI: http://scottduncombe.com/
 */
 
