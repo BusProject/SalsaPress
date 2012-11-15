BusProject/SalsaPress

commit e1d6f56d94e17fc41550ba29443e4bd53750ce83

latest version

utils/form.php
@@ -144,9 +144,9 @@ class SalsaForm {
 			// Setting up groups 
 			if( isset($this->form->PreGroup_Text) ) $form_return .= '<p>'.$this->form->PreGroup_Text.'</p>';
 
-			if( $this->form->Automatically_add_to_Groups_BOOLVALUE == 'false' && strlen($this->form->groups_KEYS) > 0   ) {
+			if( strlen($this->form->optionally_add_to_groups_KEYS) > 0   ) {
 			//If groups are optional, grabbing the group names
-				$group_pull = explode(",",$this->form->groups_KEYS);
+				$group_pull = explode(",",$this->form->optionally_add_to_groups_KEYS);
 				foreach ( $group_pull as  $thing) {
 					$i = 0;
 					if( strlen($thing) > 2 )  { 
@@ -157,10 +157,10 @@ class SalsaForm {
 						$i++;
 					}
 				}
-			} else if ( $this->form->Automatically_add_to_Groups_BOOLVALUE == 'true' ) {
-
+			}
+			if( strlen($this->form->add_to_groups_KEYS) > 0   ) {
 			// If groups are not optional, creating hidden links
-				$group_pull = explode(",",$this->form->groups_KEYS);
+				$group_pull = explode(",",$this->form->add_to_groups_KEYS);
 				foreach ( $group_pull as  $thing) {
 					if( strlen($thing) > 2 )  { 
 						$form_return .= '<input type="hidden" name="groups_KEY'.$thing.'" id="link" value="true">';

commit 93ca328c5bded792768ce802b1dc18b8a1093d86

fixing bug for groups and events
bug introduced in e1d6f56d94e17fc41550ba29443e4bd53750ce83 due to
inconsistent signup_page and event data models. Now only supports
groups if optional in events

utils/form.php
@@ -177,7 +177,7 @@ class SalsaForm {
 			// Setting up groups 
 			if( isset($this->form->PreGroup_Text) ) $form_return .= '<p>'.$this->form->PreGroup_Text.'</p>';
 
-			if( strlen($this->form->optionally_add_to_groups_KEYS) > 0   ) {
+			if( isset($this->form->optionally_add_to_groups_KEYS) && strlen($this->form->optionally_add_to_groups_KEYS) > 0  ) {
 			//If groups are optional, grabbing the group names
 				$group_pull = explode(",",$this->form->optionally_add_to_groups_KEYS);
 				foreach ( $group_pull as  $thing) {
@@ -190,8 +190,21 @@ class SalsaForm {
 						$i++;
 					}
 				}
+			} elseif ( isset($this->form->group_KEYS) && strlen($this->form->group_KEYS) > 0  ) { // Supporting events
+			//If groups are optional, grabbing the group names
+				$group_pull = explode(",",$this->form->group_KEYS);
+				foreach ( $group_pull as  $thing) {
+					$i = 0;
+					if( strlen($thing) > 2 )  { 
+						$group = $this->SalsaConnect->post('gets','object=groups&condition=groups_KEY='.$thing.'&include=Group_Name');
+						$form_return .= '<label for="'.$group['0']->Group_Name.'">'.$group['0']->Group_Name.'</label>';
+						$form_return .= '<input type="hidden" name="groups_KEY'.$group['0']->key.'" id="link" value="true">';
+						$form_return .= '<input type="checkbox" name="groups_KEY'.$group['0']->key.'_checkbox" ><br>';
+						$i++;
+					}
+				}
 			}
-			if( strlen($this->form->add_to_groups_KEYS) > 0   ) {
+			if( isset($this->form->add_to_groups_KEYS) && strlen($this->form->add_to_groups_KEYS) > 0   ) {
 			// If groups are not optional, creating hidden links
 				$group_pull = explode(",",$this->form->add_to_groups_KEYS);
 				foreach ( $group_pull as  $thing) {